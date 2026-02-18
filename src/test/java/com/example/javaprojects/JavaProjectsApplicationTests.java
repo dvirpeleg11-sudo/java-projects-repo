5c8364e764dc740b7f7c2036c81625390dbcc1b2
dvirpeleg11-sudo <dvirpeleg11@gmail.com>
2026-02-18 21:53:43 +0200

    first commit

---
+++ src/test/java/com/example/javaprojects/JavaProjectsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.javaprojects;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaProjectsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
